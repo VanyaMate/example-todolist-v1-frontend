4e26a5f693812c779bbf28e7ee35a7a4da630d9a
VanyaMate <buhu653@mail.ru>
2023-08-24 20:31:40 +0300

    try this redactor styles

--- src/pages/home-page/home-page.component.tsx
+++ src/pages/home-page/home-page.component.tsx
@@ -1,12 +1,14 @@
-import Row from "../../components/ui/containers/row/row.component";
-import React from "react";
-import MenuFixed from "../../components/menu-fixed/menu-fixed";
-import TodoContent from "../../components/todo-content/todo-content";
-import RedactorFixed from "../../components/redactor-fixed/redactor-fixed";
+import Row from '../../components/ui/containers/row/row.component';
+import React from 'react';
+import MenuFixed from '../../components/menu-fixed/menu-fixed';
+import TodoContent from '../../components/todo-content/todo-content';
+import RedactorFixed from '../../components/redactor-fixed/redactor-fixed';
+import css from './home-page.module.scss';
+
 
 const HomePage = () => {
     return (
-        <Row offset={20}>
+        <Row offset={ 20 } className={ css.container }>
             <MenuFixed/>
             <TodoContent/>
             <RedactorFixed/>
